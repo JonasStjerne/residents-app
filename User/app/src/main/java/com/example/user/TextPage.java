8855daccce08f20fbfe513a3410020a11b0ec2dd
Frederikke Filtenborg Michaelsen <frederikke.fm@icloud.com>
2022-05-01 17:50:21 +0200

    Text page and frontpage done

--- User/app/src/main/java/com/example/user/TextPage.java
+++ User/app/src/main/java/com/example/user/TextPage.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import android.util.Log;
@@ -14,13 +16,18 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Base64;
 
 import io.noties.markwon.Markwon;
 
 
 public class TextPage extends AppCompatActivity {
     TextView contentContainer;
+    ImageView pictureImage;
 
     FirebaseFirestore db;
 
@@ -29,6 +36,7 @@ public class TextPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_page);
         contentContainer = (TextView) findViewById(R.id.content);
+        pictureImage = (ImageView) findViewById(R.id.pictureImage);
 
         final Markwon markwon = Markwon.create(this);
 
@@ -44,6 +52,30 @@ public class TextPage extends AppCompatActivity {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         Log.d("TextPage", "DocumentSnapshot data: " + document.getData());
+
+                        if (document.getData().get("picture") != null) {
+                            String base64Img = document.getData().get("picture").toString();
+                            Log.d("Main", "Decode picture: " + (base64Img.length() < 60 ? base64Img : base64Img.substring(0, 59)));
+                            if (base64Img.length()>0) {
+                                if (base64Img.startsWith("data:image/jpeg;base64,")) {
+                                    base64Img = base64Img.substring("data:image/jpeg;base64,".length());
+                                }
+
+                                try {
+                                    base64Img = base64Img.replaceAll("\n", "");
+                                    byte[] imgData = Base64.getDecoder().decode(base64Img);
+                                    Bitmap image = BitmapFactory.decodeByteArray(imgData, 0, imgData.length);
+                                    pictureImage.setImageBitmap(image);
+
+                                }
+                                catch (Exception e) {
+                                    Log.e("Main", "Error decoding image", e);
+                                }
+
+                            }
+                        }
+
+
 
                         // set markdown
                         String content = document.getData().get("content").toString();
