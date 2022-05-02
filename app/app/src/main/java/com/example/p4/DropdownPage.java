6c79eb3ce5818cc909975212ca5f90ea0681afb4
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-05-02 11:44:24 +0200

    Add elements to scroll view

--- app/app/src/main/java/com/example/p4/DropdownPage.java
+++ app/app/src/main/java/com/example/p4/DropdownPage.java
@@ -3,12 +3,28 @@ package com.example.p4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DropdownPage extends AppCompatActivity {
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dropdown_page);
+
+        layout = (LinearLayout) findViewById(R.id.layout);
+    }
+
+    //Add new dropdown element. Called at onclick from plus button
+    public void addDropdown(View v){
+        TextView text = new TextView(DropdownPage.this);
+        text.setText("Test");
+
+
+        layout.addView(text);
     }
 }
