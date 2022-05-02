33284c25e27942fb54d0236ce4c580e60f8db1b2
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-05-02 12:23:56 +0200

    Add elements dynamically

--- app/app/src/main/java/com/example/p4/DropdownPage.java
+++ app/app/src/main/java/com/example/p4/DropdownPage.java
@@ -3,6 +3,7 @@ package com.example.p4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 
 public class DropdownPage extends AppCompatActivity {
     LinearLayout layout;
+    LayoutInflater inflater;
+    View dropdownElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +20,22 @@ public class DropdownPage extends AppCompatActivity {
         setContentView(R.layout.activity_dropdown_page);
 
         layout = (LinearLayout) findViewById(R.id.layout);
+
+        inflater = LayoutInflater.from(this);
+
+
+
     }
 
     //Add new dropdown element. Called at onclick from plus button
     public void addDropdown(View v){
         TextView text = new TextView(DropdownPage.this);
         text.setText("Test");
+        dropdownElement = getLayoutInflater().inflate(R.layout.dropdown_element, null, false);
+        layout.addView(dropdownElement);
+    }
 
+    public void removeDropdown(View v) {
 
-        layout.addView(text);
     }
 }
